b236072e2c0b35d0a1aeadc4ec038334cf6ee266
Etienne <etienneparmentier@protonmail.com>
2023-08-17 11:48:56 +0100

    Completed ros integration !

--- RobotCraft-Maze-Solver.ino
+++ RobotCraft-Maze-Solver.ino
@@ -1,10 +1,14 @@
 
 #include <SharpDistSensor.h>
 #include <Encoder.h>
+#include <FastLED.h>
+
 #include <ros.h>
-#include <std_msgs/Empty.h>
+
 #include <std_msgs/Float32.h>
+#include <std_msgs/UInt8MultiArray.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose2D.h>
 
 // MOTOR1 RIGHT, MOTOR2 LEFT
 /***********SENSOR SECTION*******************************/
@@ -46,10 +50,10 @@ struct RobotState {
   float x_pose, y_pose, angle_pose, x_vel, y_vel, angle_vel;
 };
 
+RobotState newstate;
+RobotState oldstate = {0, 0, 0, 0, 0, 0};
 /* This function calculate the velocities and pose of the robot using the encoders */
 struct RobotState *const calcPose(float encoderValL, float encoderValR, float dt) {
-  static RobotState oldstate;
-  RobotState newstate;
   //newstate.x_vel = ((2.0 * PI * wheel_radius) * (encoderValL + encoderValR)) / (2.0 * dt *  pulse_per_rev);
   newstate.x_vel = 2.0 * PI * wheel_radius * (encoderValL + encoderValR) / (pulse_per_rev * 2.0 * dt); 
   newstate.angle_vel = 2.0 * PI * wheel_radius * (encoderValR - encoderValL) / (pulse_per_rev * wheel_distance * dt);
@@ -170,6 +174,15 @@ void PidControl(unsigned long deltaT){
   rotateMotors(Gleft, Gright);
 }
 
+/***** Led Integration *****/
+#define NUM_LEDS 2
+#define BRIGHTNESS 64
+#define LED_TYPE WS2811
+#define COLOR_ORDER GRB
+#define LED_PIN 10 // According to presentation 3B from mechatronics craft.
+
+CRGB leds[NUM_LEDS];
+
 /***** ROS Integration *****/
 
 ros::NodeHandle  nh;
@@ -179,13 +192,30 @@ void receive_cmd(const geometry_msgs::Twist& twist_msg){
   cmd_angular = twist_msg.angular.z;
 }
 
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", receive_cmd);
+void receive_led(const std_msgs::UInt8MultiArray& led_msg){
+  leds[0] = CRGB(led_msg.data[0], led_msg.data[1], led_msg.data[2]);
+  leds[1] = CRGB(led_msg.data[3], led_msg.data[4], led_msg.data[5]);
+}
+
+void receive_pose(const geometry_msgs::Pose2D& pose_msg){
+  oldstate.x_pose = pose_msg.x;
+  oldstate.y_pose = pose_msg.y;
+  oldstate.angle_pose = pose_msg.theta;
+}
+
+ros::Subscriber<geometry_msgs::Twist> cmd_sub("cmd_vel", receive_cmd);
+ros::Subscriber<std_msgs::UInt8MultiArray> led_sub("rgb_leds", receive_led);
+ros::Subscriber<geometry_msgs::Pose2D> set_pose_sub("set_pose", receive_pose);
+
 std_msgs::Float32 ourFloat;
-ros::Publisher pub[3] = {
+ros::Publisher dist_pub[3] = {
   ros::Publisher("left_distance", &ourFloat), 
   ros::Publisher("front_distance", &ourFloat),
   ros::Publisher("right_distance", &ourFloat)
 };
+
+geometry_msgs::Pose2D ourPose;
+ros::Publisher pose_pub("pose", &ourPose);
 
 void setup() {
   initializeMotors(); // Initialize motor pins
@@ -199,10 +229,17 @@ void setup() {
 
   nh.initNode();
   nh.getHardware()->setBaud(57600);
-  nh.subscribe(sub);
+  nh.subscribe(led_sub);
+  nh.subscribe(cmd_sub);
+  nh.subscribe(set_pose_sub);
+  
   for(byte i = 0; i < 3; i++){
-    nh.advertise(pub[i]);
+    nh.advertise(dist_pub[i]);
   }
+  nh.advertise(pose_pub);
+
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
 }
 
 unsigned long interval = 100; // Interval in milliseconds (100ms = 10Hz)
@@ -218,16 +255,18 @@ void loop() {
 
   // Executed every interval
   PidControl(deltaT);
-  char buff[20];
+  
   for(byte i = 0; i < nbSensors; i++){
     distArray[i] = sensorArray[i].getDist();
     ourFloat.data = float(distArray[i]) / 1000;
-    pub[i].publish(&ourFloat);
+    dist_pub[i].publish(&ourFloat);
   }
-  // front_distance
-  // left_distance
-  // right_distance
-  // pose
+
+  ourPose.x = newstate.x_pose;
+  ourPose.y = newstate.y_pose;
+  ourPose.theta = newstate.angle_pose;
+  pose_pub.publish(&ourPose);
   
+  FastLED.show();
   nh.spinOnce();
 }
